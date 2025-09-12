dfcdbd932b8a873f93a4d78c7c9c160be9fb77a1
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-09-12 14:23:08 +0100

    Day 2.2

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-//Using 4 lines of code to  get server running with express
-
+//The address of this server connected t othe network is: http://localhost:8383
+//URL -> http://localhost:8383
+//IP -> 127.0.0.1:8383
 const express = require("express");
 const app = express();
 const PORT = 8383;
+
+console.log("Hello world!");
+
+//HTTP Verbs && Routes (or paths)
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`));
