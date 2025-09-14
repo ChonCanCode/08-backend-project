a592c6eea35ad65c71bf6f56cc4802258a59d856
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-09-14 19:44:59 +0100

    day 3

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-//The address of this server connected t othe network is: http://localhost:8383
-//URL -> http://localhost:8383
-//IP -> 127.0.0.1:8383
 const express = require("express");
 const app = express();
 const PORT = 8383;
 
 console.log("Hello world!");
 
-//HTTP Verbs && Routes (or paths)
+app.get("/", (req, res) => {
+  console.log("Hit endpoint.");
+});
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`));
