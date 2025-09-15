b4bd170e5b0efbac7b8713e23265d64db142a9f5
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-09-15 07:42:58 +0100

    day 4.1

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const PORT = 8383;
 console.log("Hello world!");
 
 app.get("/", (req, res) => {
-  console.log("Hit endpoint.");
+  console.log("Hit endpoint.", req.method);
+  res.sendStatus(200);
 });
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`));
