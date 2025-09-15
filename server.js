d559f8f040d71f65cb0692135eea4f4638dd1836
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-09-15 16:12:56 +0100

    day4.2

--- server.js
+++ server.js
@@ -2,11 +2,31 @@ const express = require("express");
 const app = express();
 const PORT = 8383;
 
-console.log("Hello world!");
+let data = {
+  name: "Hehee",
+};
 
+//Type 1 - HTML Endpoint
 app.get("/", (req, res) => {
-  console.log("Hit endpoint.", req.method);
-  res.sendStatus(200);
+  res.send(`
+    <body
+        style="background:pink;
+    color:blue;">
+    <h1>DATA:</h1>
+    <p>${JSON.stringify(data)}</p>
+    </body>
+    `);
+});
+
+app.get("/dashboard", (req, res) => {
+  console.log("Hitting dashboard endpoint");
+  res.send("Dashboard Page");
+});
+
+//Type 2 - API Endpoint (non visual)
+app.get("/api/data", (req, res) => {
+  console.log("This one is for data");
+  res.send(data);
 });
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`));
