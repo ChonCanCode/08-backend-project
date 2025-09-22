159e9eb08e2450b57ec73b5ca38c32a1edd6a2ce
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-09-22 14:29:24 +0100

    moving to database sqlite

--- Project-2/src/server.js
+++ Project-2/src/server.js
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = dirname(__filename);
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
