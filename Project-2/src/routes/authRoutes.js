4046234afbe90b6f4cf06906051c813f6bd3c41a
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-09-28 20:39:44 +0100

    P2-D8.2

--- Project-2/src/routes/authRoutes.js
+++ Project-2/src/routes/authRoutes.js
@@ -10,7 +10,21 @@ router.post("/register", (req, res) => {
   const hashedPassword = bcrypt.hashSync(password, 8);
 
   try {
-    const insertUser = db.prepare(``);
+    const insertUser = db.prepare(
+      `INSERT INTO users(username, password) VALUES (?, ?)`
+    );
+    const result = insertUser.run(username, hashedPassword);
+    const defaultTodo = `Hello! Add your first todo!`;
+    const insertTodo = db.prepare(
+      `INSERT INTO todos (user_id, task) VALUES (?, ?)`
+    );
+    insertTodo.run(result.lastInsertRowid, defaultTodo);
+    const token = jwt.sign(
+      { id: result.lastInsertRowid },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" }
+    );
+    res.json({ token });
   } catch (err) {
     console.log(err.message);
     res.sendStatus(503);
@@ -19,8 +33,22 @@ router.post("/register", (req, res) => {
 
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
-  console.log(username, password);
-  res.sendStatus(201);
+  try {
+    const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`);
+    const user = getUser.get(username);
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    if (!passwordIsValid) {
+      return res.status(401).send({ message: "Invalid password" });
+    }
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(503);
+  }
 });
 
 export default router;
